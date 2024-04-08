da4862b9ece4d9aa377eb9c693fd4023f28bbbe6
hideaki tomoda <tomoda1234567890@gmail.com>
2024-04-08 23:27:52 +0900

    clock_in clock_out

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -11,26 +11,14 @@
 
 <div class="attendance__content">
     <div class="attendance__panel">
-        <form class="attendance__button">
+        <form class="attendance__button" action="/attendance/clock-in" method="post">
+            @csrf
             <button class="attendance__button-submit" type="submit">勤務開始</button>
         </form>
-        <form class="attendance__button">
+        <form class="attendance__button" action="/attendance/clock-out" method="post">
+            @csrf
             <button class="attendance__button-submit" type="submit">勤務終了</button>
         </form>
-    </div>
-    <div class="attendance-table">
-        <table class="attendance-table__inner">
-            <tr class="attendance-table__row">
-                <th class="attendance-table__header">名前</th>
-                <th class="attendance-table__header">開始時間</th>
-                <th class="attendance-table__header">終了時間</th>
-            </tr>
-            <tr class="attendance-table__row">
-                <td class="attendance-table__item">サンプル太郎</td>
-                <td class="attendance-table__item">サンプル</td>
-                <td class="attendance-table__item">サンプル</td>
-            </tr>
-        </table>
     </div>
 </div>
 @endsection
--- src/routes/web.php
+++ src/routes/web.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AttendanceController;
+use App\Models\Attendance;
 
 Route::middleware('auth')->group(function () {
     Route::get('/', [AuthController::class, 'index']);
 });
+Route::get('/attendance', [AttendanceController::class, 'index']);
+Route::post('/attendance/clock-in', [AttendanceController::class, 'clockIn']);
+Route::post('/attendance/clock-out', [AttendanceController::class, 'clockOut']);
