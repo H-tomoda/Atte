d82a85037cbbdd939ec1504eac2ac402d4fc46ef
hideaki tomoda <tomoda1234567890@gmail.com>
2024-05-01 22:34:53 +0900

    atte修正

--- src/resources/views/atte.blade.php
+++ src/resources/views/atte.blade.php
@@ -30,12 +30,18 @@
             <td scope="col">{{ $attendance -> clock_out ??'未退勤'}}</td>
             <td scope="col">
                 @if(isset($attendance['total_break_time']))
-                {{$attendance['total_break_time']}}時間
+                {{$attendance['break_time']}}時間
                 @else
                 0 時間
                 @endif
             </td>
-            <td scope="col">勤務時間</td>
+            <td scope="col">
+                @if(isset($attendance['total_work_time']))
+                {{$attendance['total_work_time']}}時間
+                @else
+                0 時間
+                @endif
+            </td>
         </tr>
         @endforeach
         @endforeach
